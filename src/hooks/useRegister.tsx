ed00fbaaaa100614e36087cc9ceb0c6a2b6444b2
zarshab <=>
2024-04-23 13:02:53 +0500

    fix error

--- src/hooks/useRegister.tsx
+++ src/hooks/useRegister.tsx
@@ -1,24 +1,23 @@
-'use client'
-import toast from 'react-hot-toast'
-import axios from 'axios'
-import { useState } from 'react'
-const useRegister = () =>{
-const [loading,setLoading]=useState(false)
+import toast from 'react-hot-toast';
+import axios from 'axios';
+import { useState } from 'react';
 
-    const registerUser = async(data) =>{
-        setLoading(true)
-        try {
-            const resp = await axios.post('/api/user/',data);
-            setLoading(false)
-            return resp.data;
-            
-        } catch (error) {
-            setLoading(false)
-            return toast.error(error?.response?.data?.message)
-        }
+const useRegister = () => {
+  const [loading, setLoading] = useState(false);
+
+  const registerUser = async (data: any) => {
+    setLoading(true);
+    try {
+      const resp = await axios.post('/api/user/', data);
+      setLoading(false);
+      return resp.data;
+    } catch (error) {
+      setLoading(false);
+      return toast.error(error?.response?.data?.message);
     }
+  };
 
-    return {loading ,registerUser}
-}
+  return { loading, registerUser };
+};
 
 export default useRegister;
